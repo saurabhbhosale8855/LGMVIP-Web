609c198d6c820c7b64e9ad66fb4f94e863ce8aa0
Bhosale Saurabh <123993449+saurabhbhosale8855@users.noreply.github.com>
2023-04-25 00:40:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './Component/TodoList';
+function App() {
+
+  return (
+   
+
+      <>
+        <h1 className='heading'>Todo List</h1>
+        <div className="todo-list-container">
+          <TodoList />
+        
+
+      </div>
+      </>
+      );
+}
+
+      export default App;
